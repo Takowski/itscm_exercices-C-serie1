46ac7d5e863ae9f793f849753c8e6086a4c0441c
Takowski <93830775+Takowski@users.noreply.github.com>
2024-12-07 12:00:30 +0100

    correction ex9

--- exercice9/main.c
+++ exercice9/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    int a = 5, b = 6, c;
+int main() {
+
+    int a = 5, b = 6, temp;
+
     printf("valeur de a d'origine:%d\n", a);
+
     printf("valeur de b d'origine:%d\n", b);
-    c = b;
-    printf("valeur de b -> a:%d\n", b = a);
-    printf("valeur de a -> b:%d\n", a = c);
+
+    temp = b;
+
+    b = a;
+
+    printf("valeur de b -> a:%d\n", b);
+
+    a = temp;
+
+    printf("valeur de a -> b:%d\n", a);
 
     return 0;
 }
