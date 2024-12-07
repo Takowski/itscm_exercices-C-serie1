98a9818d37e96c21950e8bcf49e5a8c2f599e9ea
Takowski <93830775+Takowski@users.noreply.github.com>
2024-12-07 12:10:24 +0100

    correction ex10

--- exercice10/main.c
+++ exercice10/main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
-int a = 1, b = 2, c = 3, temp;
-int main()
-{
-    printf("Valeur de a :%d\n",a);
-    printf("Valeur de b :%d\n",b);
-    printf("Valeur de c :%d\n",c);
-    temp=b;
-    printf("Valeur de b -> a :%d\n",b=a);
-    printf("Valeur de a -> c :%d\n",a=c);
-    printf("Valeur de c -> b :%d\n",c=temp);
+
+int main() {
+
+    int a = 1, b = 2, c = 3, temp;
+
+    printf("Valeur de a :%d\n", a);
+
+    printf("Valeur de b :%d\n", b);
+
+    printf("Valeur de c :%d\n", c);
+
+    temp = b;
+
+    b = a;
+
+    printf("Valeur de b -> a :%d\n", b);
+
+    a = c;
+
+    printf("Valeur de a -> c :%d\n", a);
+
+    c = temp;
+
+    printf("Valeur de c -> b :%d\n", c);
 
     return 0;
 }
